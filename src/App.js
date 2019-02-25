a240a125f454e439539e06f01b1b77f12e676996
Tupini <piradinho1@gmail.com>
2019-02-24 16:18:54 -0800

    Add react reveal

--- src/App.js
+++ src/App.js
@@ -1,62 +1,69 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
+import Flip from "react-reveal/Flip";
 
-import Card from './Card'
-import styles from './App.module.css'
+import Card from "./Card";
+import styles from "./App.module.css";
 
-const API_URL = "https://swapi.co/api/"
+const API_URL = "https://swapi.co/api/";
 
 class App extends Component {
-  constructor(props){
-	super(props)
-	this.state = {
-	  planetData: []
-	}
-	this.nextCard = this.nextCard.bind(this)
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            planets: [],
+            cardCounter: 0
+        };
+        this.nextCard = this.nextCard.bind(this);
+    }
 
-  // Fetch 10 new planets from API
-  fetchData(){
-	  axios.get(API_URL+'planets')
-	  .then(res => {
-			let planets = res.data
-			for (let i = 0; i < 10; i++) {
-				let randomPlanet = Math.floor(Math.random() * planets.count) + 1
-				axios.get(API_URL+'planets/'+randomPlanet)
-				.then(res => {
-					let planetData = [...this.state.planetData]
-					planetData.push(res.data)
-					this.setState({planetData: planetData})
-					})
-				.catch(e => console.error(e))
-			}
-	  })
-		.catch(e => console.error(e))
-  }
+    // Fetch 10 new planets from API
+    fetchData() {
+        axios
+            .get(API_URL + "planets")
+            .then(res => {
+                let planetList = res.data;
+                let fetchPipeline = [...Array(10)].map(() => {
+                    return Math.floor(Math.random() * planetList.count) + 1;
+                });
+                let { planets } = this.state;
+                fetchPipeline.forEach(randomPlanet => {
+                    axios
+                        .get(API_URL + "planets/" + randomPlanet)
+                        .then(res => {
+                            planets = [...planets, res.data];
+                        })
+                        .finally(() => this.setState({ planets }));
+                });
+            })
+            .catch(() => alert("Could not fetch data from API."));
+    }
 
-  nextCard(){
-	let planetData = this.state.planetData
-	//Por que o ternário não funciona aqui?
-	if (planetData.length === 1) return this.fetchData()
-	planetData.shift()
-	this.setState({planetData})
-  }
+    // Remove first planet of the array
+    nextCard() {
+        let { planets, cardCounter } = this.state;
+        if (planets.length === 1) this.fetchData();
+        planets.shift();
+        this.setState({ planets, cardCounter: ++cardCounter });
+    }
 
-  componentDidMount(){
-	this.fetchData()
-  }
+    componentDidMount() {
+        this.fetchData();
+    }
 
-  render() {
-	let planet = this.state.planetData
-	let nextCard = this.nextCard
+    render() {
+        let { planets, cardCounter } = this.state;
+        let nextCard = this.nextCard;
 
-	return (
-	<div className={styles.wrapper}>
-		<Card planet={planet} />
-		<button onClick={nextCard}>Next</button>
-	</div>
-	);
-  }
+        return (
+            <div className={styles.wrapper}>
+                <Flip bottom spy={cardCounter}>
+                    <Card planet={planets[0]} heigth="800" />
+                </Flip>
+                <button onClick={nextCard}>Next</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,50 +1,46 @@
-import React from 'react'
-import ContentLoader from 'react-content-loader'
+import React from "react";
+import ContentLoader from "react-content-loader";
 
-import styles from './Card.module.css'
+import styles from "./Card.module.css";
 
 const LightSaberLoading = () => {
-
     return (
         <ContentLoader
             ariaLabel={false}
-		    height={20}
-		    width={200}
-		    speed={1}
-		    primaryColor="#333333"
-		    secondaryColor="#781515"
-	>   
-		    <rect x="52.92" y="5.67" rx="4" ry="4" width="125" height="10.21" /> 
-		    <rect x="25.69" y="5.67" rx="2" ry="2" width="25" height="10.21" />
-	    </ContentLoader>
-    )
-}
+            height={20}
+            width={200}
+            speed={1}
+            primaryColor="#333333"
+            secondaryColor="#781515"
+        >
+            <rect x="52.92" y="5.67" rx="4" ry="4" width="125" height="10.21" />
+            <rect x="25.69" y="5.67" rx="2" ry="2" width="25" height="10.21" />
+        </ContentLoader>
+    );
+};
 
 const ListLoading = () => {
-    
     return (
-    
-        <ContentLoader 
+        <ContentLoader
             ariaLabel={false}
-	    	height={200}
-	    	width={300}
-	    	speed={1}
-	    	primaryColor="#333333"
-	    	secondaryColor="#111111"
-	    >
-	    	<circle cx="10" cy="20" r="8" /> 
-	    	<rect x="25" y="15" rx="5" ry="5" width="220" height="10" /> 
-	    	<circle cx="10" cy="75" r="8" /> 
-	    	<rect x="25" y="70" rx="5" ry="5" width="220" height="10" /> 
-	    	<circle cx="10" cy="130" r="8" /> 
-	    	<rect x="25" y="125" rx="5" ry="5" width="220" height="10" />
-	    </ContentLoader>
-    )
-}
+            height={200}
+            width={300}
+            speed={1}
+            primaryColor="#333333"
+            secondaryColor="#111111"
+        >
+            <circle cx="10" cy="20" r="8" />
+            <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
+            <circle cx="10" cy="75" r="8" />
+            <rect x="25" y="70" rx="5" ry="5" width="220" height="10" />
+            <circle cx="10" cy="130" r="8" />
+            <rect x="25" y="125" rx="5" ry="5" width="220" height="10" />
+        </ContentLoader>
+    );
+};
 
 const EmptyCard = props => {
-
-    return(
+    return (
         <div className={styles.container}>
             <div className={styles.title}>
                 <LightSaberLoading />
@@ -53,30 +49,38 @@ const EmptyCard = props => {
                 <ListLoading />
             </div>
         </div>
-    )
-}
+    );
+};
 
 const Card = props => {
-
-    let planet = props.planet[0]
-    return props.planet.length?
-    (
-        <div className={styles.container}>
+    let planet = props.planet;
+    return props.planet ? (
+        <div className={styles.container} style={{ minHeight: props.height }}>
             <div className={styles.title}>
-                <h1>
-                {planet.name}</h1>
+                <h1>{planet.name}</h1>
             </div>
             <div className={styles.content}>
                 <ul>
-                    <li><strong>Population:</strong> {planet.population}</li>
-                    <li><strong>Climate:</strong> {planet.climate}</li>
-                    <li><strong>Terrain:</strong> {planet.terrain}</li>
-                    <li className={styles.featured}>Featured in <strong>{planet.films.length}</strong> {planet.films.length > 1 ? 'films':'film'}</li>
+                    <li>
+                        <strong>Population:</strong> {planet.population}
+                    </li>
+                    <li>
+                        <strong>Climate:</strong> {planet.climate}
+                    </li>
+                    <li>
+                        <strong>Terrain:</strong> {planet.terrain}
+                    </li>
+                    <li className={styles.featured}>
+                        Featured in{" "}
+                        <strong>{planet.films.length || "no"} </strong>
+                        {planet.films.length !== 1 ? "films" : "film"}
+                    </li>
                 </ul>
             </div>
         </div>
-    )
-    :<EmptyCard />
-}
+    ) : (
+        <EmptyCard />
+    );
+};
 
-export default Card
+export default Card;
